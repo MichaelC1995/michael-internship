530db9cd4f2512efc965ab492115a043c4822260
MichaelC1995 <m.chatellier@yahoo.com>
2025-05-20 01:32:16 -0500

    explore page functionality

--- src/components/UI/NewItemsSkeleton.jsx
+++ src/components/UI/NewItemsSkeleton.jsx
@@ -1,117 +1,31 @@
 import React from 'react'
-import Slider from "react-slick";
+
 
 const NewItemsSkeleton = () => {
-    const settings = {
-        arrows: true,
-        dots: true,
-        lazyLoad: true,
-        infinite: true,
-        speed: 500,
-        slidesToShow: 4,
-        slidesToScroll: 4,
-        initialSlide: 0,
-        nextArrow: <SampleNextArrow/>,
-        prevArrow: <SamplePrevArrow/>,
-        responsive: [
-            {
-                breakpoint: 1024,
-                settings: {
-                    slidesToShow: 3,
-                    slidesToScroll: 3,
-                    infinite: true,
-                    dots: true
-                }
-            },
-            {
-                breakpoint: 600,
-                settings: {
-                    slidesToShow: 2,
-                    slidesToScroll: 2,
-                }
-            },
-            {
-                breakpoint: 480,
-                settings: {
-                    slidesToShow: 1,
-                    slidesToScroll: 1
-                }
-            },
-        ]
-    }
-
-    function SamplePrevArrow(props) {
-        const {className, style, onClick} = props;
-        return (
-            <div
-                className={className}
-                style={{...style, display: "block", background: "gray", borderRadius: "50%"}}
-                onClick={onClick}
-            />
-        );
-    }
-
-    function SampleNextArrow(props) {
-        const {className, style, onClick} = props;
-        return (
-            <div
-                className={className}
-                style={{...style, display: "block", background: "gray", borderRadius: "50%"}}
-                onClick={onClick}
-            />
-        );
-    }
+    const skeletonItems = Array.from({ length: 4 }, (_, index) => index);
 
     return (
-        <div className={"slider-container"}>
-            <Slider {...settings}>
-                <div>
-                    <div className="nft__item" style={{height: "441px"}}>
-                        <div className="nft__item_wrap skeleton">
-                            <div className="nft__item_extra">
-                            </div>
-                            <div className="lazy nft__item_preview skeleton" alt=""/>
-                        </div>
-                        <div className="nft__item_info skeleton"
-                             style={{marginTop: "10px", width: "70%"}}>
-                            <div className="nft__item_price skeleton-text"></div>
-                            <div className="nft__item_price skeleton-text"></div>
-                            <div className="nft__item_like">
-                                <span className={"skeleton-text"}></span>
-                            </div>
-                        </div>
-                        <div className="nft__item_info skeleton"
-                             style={{marginTop: "10px", width: "50%", position: "relative"}}>
-                            <div className="nft__item_price skeleton-text"></div>
-                            <div className="nft__item_price skeleton-text"></div>
-                            <span className={"skeleton-text"}></span>
+        <div className="skeleton-container">
+            {skeletonItems.map((item) => (
+                <div key={item} className="nft__item nft__item__skeleton" style={{ height: "441px" }}>
+                    <div className="nft__item_wrap skeleton">
+                        <div className="nft__item_extra"></div>
+                        <div className="lazy nft__item_preview skeleton" alt="" />
+                    </div>
+                    <div className="nft__item_info skeleton" style={{ marginTop: "10px", width: "70%" }}>
+                        <div className="nft__item_price skeleton-text"></div>
+                        <div className="nft__item_price skeleton-text"></div>
+                        <div className="nft__item_like">
+                            <span className="skeleton-text"></span>
                         </div>
                     </div>
-                </div>
-                <div>
-                    <div className="nft__item" style={{height: "441px"}}>
-                        <div className="nft__item_wrap skeleton">
-                            <div className="nft__item_extra">
-                            </div>
-                            <div className="lazy nft__item_preview skeleton" alt=""/>
-                        </div>
-                        <div className="nft__item_info skeleton"
-                             style={{marginTop: "10px", width: "70%"}}>
-                            <div className="nft__item_price skeleton-text"></div>
-                            <div className="nft__item_price skeleton-text"></div>
-                            <div className="nft__item_like">
-                                <span className={"skeleton-text"}></span>
-                            </div>
-                        </div>
-                        <div className="nft__item_info skeleton"
-                             style={{marginTop: "10px", width: "50%", position: "relative"}}>
-                            <div className="nft__item_price skeleton-text"></div>
-                            <div className="nft__item_price skeleton-text"></div>
-                            <span className={"skeleton-text"}></span>
-                        </div>
+                    <div className="nft__item_info skeleton" style={{ marginTop: "10px", width: "50%", position: "relative" }}>
+                        <div className="nft__item_price skeleton-text"></div>
+                        <div className="nft__item_price skeleton-text"></div>
+                        <span className="skeleton-text"></span>
                     </div>
                 </div>
-            </Slider>
+            ))}
         </div>
     )
 }
--- src/components/explore/ExploreItems.jsx
+++ src/components/explore/ExploreItems.jsx
@@ -1,80 +1,120 @@
-import React from "react";
-import { Link } from "react-router-dom";
-import AuthorImage from "../../images/author_thumbnail.jpg";
-import nftImage from "../../images/nftImage.jpg";
+import React, {useEffect, useState} from "react";
+import {Link} from "react-router-dom";
+import axios from "axios";
+import Timer from "../UI/Timer";
+import NewItemsSkeleton from "../UI/NewItemsSkeleton";
 
-const ExploreItems = () => {
-  return (
-    <>
-      <div>
-        <select id="filter-items" defaultValue="">
-          <option value="">Default</option>
-          <option value="price_low_to_high">Price, Low to High</option>
-          <option value="price_high_to_low">Price, High to Low</option>
-          <option value="likes_high_to_low">Most liked</option>
-        </select>
-      </div>
-      {new Array(8).fill(0).map((_, index) => (
-        <div
-          key={index}
-          className="d-item col-lg-3 col-md-6 col-sm-6 col-xs-12"
-          style={{ display: "block", backgroundSize: "cover" }}
-        >
-          <div className="nft__item">
-            <div className="author_list_pp">
-              <Link
-                to="/author"
-                data-bs-toggle="tooltip"
-                data-bs-placement="top"
-              >
-                <img className="lazy" src={AuthorImage} alt="" />
-                <i className="fa fa-check"></i>
-              </Link>
-            </div>
-            <div className="de_countdown">5h 30m 32s</div>
+const ExploreItems = ({data}) => {
+    const [isLoading, setLoading] = useState(true)
+    const [exploreHTML, setExploreHTML] = useState([])
+    const [visibleCount, setVisibleCount] = useState(8)
+    const [selectedFilter, setSelectedFilter] = useState("")
 
-            <div className="nft__item_wrap">
-              <div className="nft__item_extra">
-                <div className="nft__item_buttons">
-                  <button>Buy Now</button>
-                  <div className="nft__item_share">
-                    <h4>Share</h4>
-                    <a href="" target="_blank" rel="noreferrer">
-                      <i className="fa fa-facebook fa-lg"></i>
-                    </a>
-                    <a href="" target="_blank" rel="noreferrer">
-                      <i className="fa fa-twitter fa-lg"></i>
-                    </a>
-                    <a href="">
-                      <i className="fa fa-envelope fa-lg"></i>
-                    </a>
-                  </div>
-                </div>
-              </div>
-              <Link to="/item-details">
-                <img src={nftImage} className="lazy nft__item_preview" alt="" />
-              </Link>
+    useEffect(() => {
+        async function renderExplore() {
+            try {
+                const {data} = await axios.get(`https://us-central1-nft-cloud-functions.cloudfunctions.net/explore?filter=${selectedFilter}`)
+                setExploreHTML(data)
+            } catch (error) {
+                console.log("Error fetching renderExplore", error)
+            } finally {
+                setLoading(false)
+            }
+        }
+        renderExplore()
+    }, [selectedFilter])
+
+    const loadMoreItems = () => {
+        setVisibleCount(prevCount => prevCount + 4)
+    }
+
+    const handleFilterChange = (event) => {
+        setSelectedFilter(event.target.value)
+        setLoading(true)
+    }
+
+    return (
+        <>
+            <div>
+                <select id="filter-items" onChange={handleFilterChange} defaultValue="">
+                    <option value="">Default</option>
+                    <option value="price_low_to_high">Price, Low to High</option>
+                    <option value="price_high_to_low">Price, High to Low</option>
+                    <option value="likes_high_to_low">Most liked</option>
+                </select>
             </div>
-            <div className="nft__item_info">
-              <Link to="/item-details">
-                <h4>Pinky Ocean</h4>
-              </Link>
-              <div className="nft__item_price">1.74 ETH</div>
-              <div className="nft__item_like">
-                <i className="fa fa-heart"></i>
-                <span>69</span>
-              </div>
+
+            {isLoading ? (
+
+                <NewItemsSkeleton/>
+
+            ) : (
+                    exploreHTML.slice(0, visibleCount).map((data) => (
+                        <div
+                            key={data.id}
+                            className="d-item col-lg-3 col-md-6 col-sm-6 col-xs-12"
+                            style={{display: "block", backgroundSize: "cover"}}
+                        >
+                            <div className="nft__item">
+                                <div className="author_list_pp">
+                                    <Link
+                                        to={`/author/${data.authorId}`}
+                                        data-bs-toggle="tooltip"
+                                        data-bs-placement="top"
+                                    >
+                                        <img className="lazy" src={data.authorImage} alt=""/>
+                                        <i className="fa fa-check"></i>
+                                    </Link>
+                                </div>
+
+                                {data.expiryDate && (
+                                    <div className="de_countdown"><Timer duration={data.expiryDate}/></div>
+                                )}
+
+                                <div className="nft__item_wrap">
+                                    <div className="nft__item_extra">
+                                        <div className="nft__item_buttons">
+                                            <button>Buy Now</button>
+                                            <div className="nft__item_share">
+                                                <h4>Share</h4>
+                                                <a href="" target="_blank" rel="noreferrer">
+                                                    <i className="fa fa-facebook fa-lg"></i>
+                                                </a>
+                                                <a href="" target="_blank" rel="noreferrer">
+                                                    <i className="fa fa-twitter fa-lg"></i>
+                                                </a>
+                                                <a href="">
+                                                    <i className="fa fa-envelope fa-lg"></i>
+                                                </a>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <Link to={`/item-details/${data.nftId}`}>
+                                        <img src={data.nftImage} className="lazy nft__item_preview" alt=""/>
+                                    </Link>
+                                </div>
+                                <div className="nft__item_info">
+                                    <Link to="/item-details">
+                                        <h4>{data.title}</h4>
+                                    </Link>
+                                    <div className="nft__item_price">{data.price} ETH</div>
+                                    <div className="nft__item_like">
+                                        <i className="fa fa-heart"></i>
+                                        <span>{data.likes}</span>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    ))
+            )}
+
+            <div className="col-md-12 text-center">
+                <button onClick={loadMoreItems} id="loadmore" className="btn-main lead">
+                    Load more
+                </button>
             </div>
-          </div>
-        </div>
-      ))}
-      <div className="col-md-12 text-center">
-        <Link to="" id="loadmore" className="btn-main lead">
-          Load more
-        </Link>
-      </div>
-    </>
-  );
+        </>
+    );
 };
 
 export default ExploreItems;
